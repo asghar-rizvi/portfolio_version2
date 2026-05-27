32391c9bcb926a8aee8c8374223fb5115e718e65
asghar-rizvi <asgharqamberrozvi@gmail.com>
2026-05-27 20:07:06 +0500

    completed portfolio with build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio_updated/", 
+  base: "/portfolio_version2/", 
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
